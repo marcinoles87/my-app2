3ef6add7f04521333e6e0844047a9a8ebd0f89ad
marcinoles87 <marcinoles87@gmail.com>
2026-05-08 21:32:37 +0200

    add code

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,7 +2,22 @@ import React from 'react'
 
 function Nav() {
   return (
-    <div>Nav</div>
+    <div className='navbar'>
+
+        <div className='navbar-logo'>
+            <img src="" alt="" />
+        </div>
+
+        <div className='navbar-elements'>
+            <ul>
+                <li>O Nas</li>
+                <li>Promocje</li>
+                <li>Produkty</li>
+                <li>Nasze marki</li>
+                <li>Kontakt</li>
+            </ul>
+        </div>
+    </div>
   )
 }
 
