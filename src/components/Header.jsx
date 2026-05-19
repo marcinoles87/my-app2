76cb004c9438039d5fb53e12070d44e90efa0bbd
marcinoles87 <marcinoles87@gmail.com>
2026-05-19 20:11:48 +0200

    add code

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
+import Database from '../Database/Database'
 
 function Header() {
   return (
     <div className='header-container'>
         <div className='header-elements'>
-
+            <h1>Produkty w loopie</h1>
         </div>
+
+        <div className='header-product'>
+          <Database></Database>
+        </div>
+
     </div>
   )
 }
