707be1c57a451cc5b799c6bbb7c88253732a1ff2
marcinoles87 <marcinoles87@gmail.com>
2026-05-19 20:20:48 +0200

    add code

--- src/Database/Database.jsx
+++ src/Database/Database.jsx
@@ -1,8 +1,52 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Database() {
+
+    const [database,setDatabase] = useState([])
+
+    const database2 = [
+        {
+            img:'',
+            Product:'test1',
+            Nazwa:'test1',
+            Cena:'14.99'
+
+        },
+
+        {
+            img:'',
+            Product:'test1',
+            Nazwa:'test1',
+            Cena:'14.99'
+
+        },
+
+        {
+            img:'',
+            product:'test1',
+            nazwa:'test1',
+            cena:'14.99'
+
+        },
+        
+    ]
+
   return (
-    <div>Database</div>
+    <div className='database-product'>
+        {database2.map( (item,index) => {
+            return(
+                <div className='database-element' key={index}>
+                    <img src="" alt="" />
+                    <h3>Produkt</h3>
+                    <p>Nazwa</p>
+                    <h4>Cena  {item.cena}</h4>
+
+
+                    </div>
+            )
+        })}
+
+    </div>
   )
 }
 
