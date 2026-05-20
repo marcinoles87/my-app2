040b179135ce62e691cbaa528689a609fe8485dd
marcinoles87 <marcinoles87@gmail.com>
2026-05-20 18:57:22 +0200

    add code

--- src/pages/Kontakt.jsx
+++ src/pages/Kontakt.jsx
@@ -2,7 +2,22 @@ import React from 'react'
 
 function Kontakt() {
   return (
-    <div>Kontakt</div>
+    <div className='kontakt-container'>
+      <div className='kontakt-szybki'>
+        <h1 style={{fontWeight:'lighter'}}>Szybki kontakt</h1>
+        <p>whatsup : 22342455</p>
+        <br/>
+        <p>Napisz do nas :</p>
+        <a style={{textDecoration:'none'}} href='mrc87@gmail.com'>mrc87@gmail.com</a>
+
+      </div>
+
+      <div className='kontakt-form'>
+        <form>
+
+        </form>
+      </div>
+    </div>
   )
 }
 
