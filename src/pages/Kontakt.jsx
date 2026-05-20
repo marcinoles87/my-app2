7514f1b96b6602fe7022ac5d7aa25956f83f0714
marcinoles87 <marcinoles87@gmail.com>
2026-05-20 19:06:47 +0200

    add code

--- src/pages/Kontakt.jsx
+++ src/pages/Kontakt.jsx
@@ -4,9 +4,8 @@ function Kontakt() {
   return (
     <div className='kontakt-container'>
       <div className='kontakt-szybki'>
-        <h1 style={{fontWeight:'lighter'}}>Szybki kontakt</h1>
+        <h1 style={{fontWeight:'lighter',color:'black'}}>Szybki kontakt</h1>
         <p>whatsup : 22342455</p>
-        <br/>
         <p>Napisz do nas :</p>
         <a style={{textDecoration:'none'}} href='mrc87@gmail.com'>mrc87@gmail.com</a>
 
@@ -14,6 +13,21 @@ function Kontakt() {
 
       <div className='kontakt-form'>
         <form>
+          <h1>Kontakt z Nami</h1>
+          <div className='form-group'>
+              <label style={{color:'#212529'}}>Temat</label>
+              <input type="text" />
+          </div>
+          <div className='form-group'>
+              <label style={{color:'#212529'}}>Adres</label>
+              <input type="email" />
+          </div>
+          <div className='form-group'>
+              <label style={{color:'#212529'}}>Wiadomość</label>
+              <input type="text" />
+          </div>
+
+          <button>Wyślij</button>
 
         </form>
       </div>
