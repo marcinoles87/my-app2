28eb539c79e345ce8e78a0436f98c774362bb467
marcinoles87 <marcinoles87@gmail.com>
2026-05-20 18:47:47 +0200

    add code

--- src/pages/Wysyłka.jsx
+++ src/pages/Wysyłka.jsx
@@ -9,13 +9,12 @@ function Wysyłka() {
         <h3 style={{color:'#333399'}}>Dostawa powyżej 250 zł - gratis !</h3>
       </div>
 
-      <div>
-        <p><span style={{color:'#333399'}}>Dostawa zam&oacute;wionego towaru zostanie zrealizowana tylko wtedy, kiedy zamawiany towar znajduje się w&nbsp;Magazynie Sklepu internetowego megapunkt.pl.</span></p>
+      <div className='wysylka-info'>
+        <p><span style={{color:'#333399'}}>Dostawa zam&oacute;wionego towaru zostanie zrealizowana tylko wtedy, kiedy zamawiany towar znajduje się fizycznie na stanach magazynu</span></p>
           <div>&nbsp;</div>
           <div>Przedmiot zam&oacute;wienia może być dostarczony do Klienta w&nbsp;zależności od jego wyboru:</div>
-          <div>(a) na adres wskazany przez Klienta w&nbsp;zam&oacute;wieniu. Towar dostarczany jest przez Firmę kurierską FEDEX Sp. z&nbsp;o.o.</div>
-          <div>(b) do wskazanego przez Klienta w&nbsp;sklepu sieci Megapunkt.</div>
-          <div>(c) do wybranego Paczkomatu firmy Inpost.</div>
+          <div>a) na adres wskazany przez Klienta w&nbsp;zam&oacute;wieniu. Towar dostarczany jest przez Firmę kurierską FEDEX / DPD</div>
+          <div>b) do wybranego Paczkomatu firmy Inpost.</div>
           <div>&nbsp;</div>
           <p><strong>Aktualny cennik przesyłek kurierskich</strong>&nbsp;jest następujący:</p>
           <div>a) <strong>17 zł</strong>&nbsp;płatne przelewem</div>
