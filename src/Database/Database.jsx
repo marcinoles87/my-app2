90477108a894537ebf28298b4f00f9b1e40e76bd
marcinoles87 <marcinoles87@gmail.com>
2026-05-21 21:04:21 +0200

    add code

--- src/Database/Database.jsx
+++ src/Database/Database.jsx
@@ -5,6 +5,7 @@ import logoHeader from '../media/logo_header.png'
 function Database() {
 
     const [database,setDatabase] = useState([])
+    
 
     const database2 = [
         {
@@ -106,8 +107,7 @@ function Database() {
     ]
 
     const handleAddProduct = (item) =>{
-        console.log(item.index)
-
+        
     }
 
   return (
@@ -122,7 +122,7 @@ function Database() {
                          <h3>Produkt</h3>
                          <p>Nazwa</p>
                          <h4>Cena  {item.cena}</h4>
-                         <button onClick={() => handleAddProduct(index)}>Dodaj</button>
+                         <button onClick={() => handleAddProduct(item)}>Dodaj</button>
                     </div>
                     
                    
