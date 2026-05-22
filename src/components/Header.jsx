4b053c2b408e0a8d4cde5287770677cadd4fc6cc
marcinoles87 <marcinoles87@gmail.com>
2026-05-22 22:11:29 +0200

    add code

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,25 @@
 import React from 'react'
 import Database from '../Database/Database'
+import kai from '../media/kaiLoop.png'
+import wl from '../media/wlLoop.png'
+import ifg from '../media/ifLoop.png'
 
 function Header() {
+
+  const imagesLoop =  [{kai},{wl},{ifg}]
+  let index = 0;
+
+   setInterval(() => {
+    
+  }, 2000);
+
+
   return (
     <div className='header-container'>
         <div className='header-elements'>
             <h1>Produkty w loopie</h1>
+            <img id="slideshow" src="img1.jpg" width="400" alt='loopimg'/>
+
         </div>
 
         <div className='header-product'>
