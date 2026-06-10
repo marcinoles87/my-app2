72c5b502281c6283aaeafd2fa664d6b82c32db93
marcinoles87 <marcinoles87@gmail.com>
2026-06-10 18:58:13 +0200

    add code

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
 import MainWydarzenie from '../pages/MainWydarzenie'
-import {Link} from 'react-router'
 
 function Main() {
   return (
     <div className='main-container'>
       <div className='main-element'>
-        <Link to={'/mainWydarzenie'}>Link</Link>
         
 
       </div>
