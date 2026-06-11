d1afdf8d4dfb74793dad9736f5ff93522cfeed1a
marcinoles87 <marcinoles87@gmail.com>
2026-06-11 16:44:49 +0200

    add code

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -6,16 +6,19 @@ function Main() {
     <div className='main-container'>
       <div className='main-element-one'>
         <Link to={'/mainWydarzenie'}></Link>
+        <p>Kuchnia</p>
         
       </div>
 
       <div className='main-element-two'>
         <Link to={'/mainWydarzenie'}>-</Link>
+        <p>Łazienka</p>
         
       </div>
 
       <div className='main-element-three'>
         <Link to={'/mainWydarzenie'}>-</Link>
+        <p>HoReCa</p>
         
       </div>
 
