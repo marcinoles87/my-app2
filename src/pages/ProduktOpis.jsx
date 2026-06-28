4857530af55b5bfb2e900a4bd5fb7aa7fa9ce834
marcinoles87 <marcinoles87@gmail.com>
2026-06-28 10:05:58 +0200

    add code

--- src/pages/ProduktOpis.jsx
+++ src/pages/ProduktOpis.jsx
@@ -3,10 +3,25 @@ import React from 'react'
 function ProduktOpis({produktOpis}) {
     console.log(produktOpis)
   return (
+  
     <div>
-        <p>{produktOpis.nazwa}</p>
-        {produktOpis.cena}
+
+        
+        
+        <div className='database-element'>
+                <div className='database-element-img'>
+                       <img src={produktOpis.img} alt="" /> 
+                </div>
+                <div className='database-element-info'>
+                        <div className='database-element-info2'>
+                            <p>{produktOpis.nazwa}</p>
+                            <h2 style={{color:'rgb(0,0,190'}}> {produktOpis.cena},00 PLN</h2> 
+                        </div>
+
+                </div>
+                </div>
     </div>
+ 
   )
 }
 
