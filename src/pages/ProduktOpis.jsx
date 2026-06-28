a54fae04df9dc87281b4b4f46dda46ee358a9419
marcinoles87 <marcinoles87@gmail.com>
2026-06-28 10:12:16 +0200

    add code

--- src/pages/ProduktOpis.jsx
+++ src/pages/ProduktOpis.jsx
@@ -4,22 +4,29 @@ function ProduktOpis({produktOpis}) {
     console.log(produktOpis)
   return (
   
-    <div>
-
-        
-        
+    <div className='produkt-opis-container'>
         <div className='database-element'>
                 <div className='database-element-img'>
                        <img src={produktOpis.img} alt="" /> 
                 </div>
                 <div className='database-element-info'>
                         <div className='database-element-info2'>
-                            <p>{produktOpis.nazwa}</p>
-                            <h2 style={{color:'rgb(0,0,190'}}> {produktOpis.cena},00 PLN</h2> 
+                            
+                            
                         </div>
 
                 </div>
-                </div>
+        </div>
+
+        <div className='produkt-opis'>
+          <h1>{produktOpis.nazwa}</h1>
+          
+          <p>Marka : </p>
+          <p>Kod produktu : {produktOpis.product}</p>
+          <h2 style={{color:'rgb(0,0,190'}}> {produktOpis.cena},00 PLN</h2> 
+          <p>Opis : </p>
+
+        </div>
     </div>
  
   )
