1a5e8a1e352886d4d0ebec8ea4c4a9491524b425
marcinoles87 <marcinoles87@gmail.com>
2026-09-01 21:52:07 +0200

    add code

--- src/Database/Database.jsx
+++ src/Database/Database.jsx
@@ -185,32 +185,25 @@ const database2 = [
 
     }
 
-    // const paginationAdd = () =>{
+    const paginationAdd = () =>{
 
-    //   console.log('w srodku')
+      console.log('w srodku')
     
-    //     const databasePagination = database2
-    //     let databaseLength = databasePagination.length
+        const databasePagination = database2
+        let databaseLength = databasePagination.length
 
-    //             const p1 = databasePagination.slice(0,4)
-    //             const p2 = databasePagination.slice(4,8)
-    //             const p3 = databasePagination.slice(8,12)
-    //             const p4 = databasePagination.slice(12,17)
+        const e1 = databasePagination.slice(0,10)
+        const e2 = databasePagination.toSpliced(11,18)
+        
 
-          
-    //        const mapPag = []
+        setDatabase(e1)
+        if(databaseLength<4){
+          setDatabase(database2)
+        }
 
-    //        setPagiantion(mapPag)
-
-    //       for( let i = 0; i<=databaseLength/4 ; i++){
-    //         const minArray = i*4
-    //         const maxArray = minArray+4
-    //         const e1 = databasePagination.slice(minArray,maxArray)
-    //         mapPag.push( <button onClick={ (e) => setDatabase(e1)}>{i}</button>)
-
-    //       }
-
-    //     }
+       
+ 
+        }
 
 
   return (
@@ -240,7 +233,7 @@ const database2 = [
             )
         })}
 
-        <button>Next Page</button>
+        <button onClick={ () =>paginationAdd()}>Next Page</button>
 
         </>
 
