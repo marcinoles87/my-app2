73f783779e25ac456f8c3cb6312d3dec9f8f5d80
marcinoles87 <marcinoles87@gmail.com>
2026-09-12 18:57:06 +0200

    add code

--- src/pages/Produkty.jsx
+++ src/pages/Produkty.jsx
@@ -1,10 +1,39 @@
 import React from 'react'
+import {Link} from 'react-router'
 
 function Produkty({setDatabase,database}) {
   console.log(database)
+
+  const handleAddProduct = (item) =>{
+
+
+    }
   return (
     <div className='produkty-container'>
-      Produkty</div>
+      <div className='produkty'>
+         {database.map( (item,index) => {
+            return(
+                <div className='database-element' key={index}>
+                    <div className='database-element-img'>
+                       <Link to={'produktOpis'}  ><img src={item.img} alt="" /> </Link>
+                       
+                    </div>
+                    <div className='database-element-info'>
+                        <div className='database-element-info2'>
+                            <h3>{item.nazwa}</h3>
+                            <h2 style={{color:'rgb(0,0,190'}}>{Math.round(item.cena-(item.cena*20/100))} zł</h2> 
+                            <p style={{padding:'10px 0px'}}>Cena regularna : <span style={{textDecoration:'line-through'}}>{item.cena}</span></p>
+
+                        </div>
+                        
+                            <button onClick={() => handleAddProduct(item)}>Dodaj</button>
+                            {/* <button onClick={() => handlePorownaj(item)} style={{backgroundColor:'rgb(29, 118, 151)'}}>Porównaj</button> */}
+                    </div>
+                </div>
+            )
+        })}
+      </div>
+      </div>
   )
 }
 
